c9bf29bc1c84ab67cd9c3596bcd8de99190f20e5
ccccrrrr <zcr1006@gmail.com>
2021-05-23 15:13:44 +0800

    test

--- start.c
+++ start.c
@@ -6,9 +6,16 @@ extern int optind;   //下一次调用getopt的时，从optind存储的位置处
 extern int opterr;  //当opterr=0时，getopt不向stderr输出错误信息。
 extern int optopt;  //当命令行选项字符不包括
 
-void hasServerGetHostByName(char* selectServer, char* hostname) {
-    ngethostbyname(hostname, selectServer, 1);
-    return;
+void myGetHostByName(char* selectServer, char* hostname, int trace_or_not, int select_or_not) {
+    if(trace_or_not == 0 && select_or_not == 0) {
+        ngethostbyname(hostname, "", T_A);
+    }else if(trace_or_not * select_or_not == 1) {
+        ngethostbyname_trace(hostname, selectServer, 1);
+    }else if(trace_or_not == 1) {
+        ngethostbyname_trace(hostname, "", 1);
+    }else if(select_or_not == 1) {
+        ngethostbyname(hostname, selectServer, 1);
+    }
 }
 
 int main(int argc, char *argv[]) {
@@ -24,25 +31,28 @@ int main(int argc, char *argv[]) {
         return 0;
     }
     if(argc > 2) {
+        int trace_or_not = 0;
+        int select_or_not = 0;
+        char selectServer[20] = {0};
         while ((c = getopt(argc, argv, "s:t")) != -1) {
             switch (c) {
                 case 's': /* @server */
-                    hasServerGetHostByName(optarg, target);
-                    // printf("s: %s\n", optarg);
+                    select_or_not = 1;
+                    strcpy(selectServer, optarg);
                     break;
                 case 't': /* trace */
-                    printf("t\n");
-                    ngethostbyname_trace(target, "", 1);
+                    trace_or_not = 1;
                     break;
                 default:
-                    ngethostbyname(target, "", 1);
+                    // ngethostbyname(target, "", 1);
                     printf("default %s\n", optarg);
                     break;            
             }
         }
+        myGetHostByName(selectServer, target, trace_or_not, select_or_not);
     } else {
         printf("target: %s\n", target);
-        /* default server */
+        /* default dns search */
         ngethostbyname(target, "", 1);
     }
 }
